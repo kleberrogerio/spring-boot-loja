b9d54744053be69349863781c21b5460a42b5665
alexandreeufrazio <alee.ram@hotmail.com>
2021-09-01 01:07:45 +0000

    Atualização VendaRepositoryTests

--- src/test/java/br/gov/sp/fatec/springbootloja/repository/VendaRepositoryTests.java
+++ src/test/java/br/gov/sp/fatec/springbootloja/repository/VendaRepositoryTests.java
@@ -3,14 +3,16 @@ package br.gov.sp.fatec.springbootloja.repository;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.math.BigDecimal;
-
+import java.util.Date;
+import java.util.HashSet;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
 import br.gov.sp.fatec.springbootloja.entity.Venda;
+import br.gov.sp.fatec.springbootloja.entity.Produto;
+import br.gov.sp.fatec.springbootloja.entity.Marca;
 import br.gov.sp.fatec.springbootloja.service.VendaService;
 
 @SpringBootTest
@@ -22,14 +24,48 @@ public class VendaRepositoryTests {
     private VendaRepository vendaRepo;
 
     @Autowired
+    private ProdutoRepository produtoRepo;
+
+    @Autowired
+    private MarcaRepository marcaRepo;
+
+    @Autowired
     private VendaService vendaService;
 
     @Test
 	void vendaServiceFindById() {
 		Venda venda = vendaService.cadastrarVenda(1L, new BigDecimal("9500.00"), "DESKTOP");
 
-        assertNotNull(vendaRepo.findById(1L));	   
+        assertNotNull(vendaRepo.findById(venda.getId()));	   
 		
 	}
+
+    @Test
+	void testaVendaInsercaoTestOk() {
+		Venda venda = new Venda();
+
+        Marca marca = new Marca();
+
+		Produto produto = new Produto();
+
+        Date date = new Date();
+
+		marca.setNome("Logitech");
+		marcaRepo.save(marca);
+
+		produto.setNome("Mouse");
+		produto.setPreco(new BigDecimal("150.00"));
+		produto.setMarca(marca);
+        produtoRepo.save(produto);
+
+        venda.setData(date);
+        venda.setPrecoVenda(new BigDecimal("180.00"));
+        venda.setQtde(2L);
+        venda.setProdutos(new HashSet<Produto>());
+        venda.getProdutos().add(produto);
+        vendaRepo.save(venda);
+
+		assertNotNull(venda.getId());
+    }
     
 }

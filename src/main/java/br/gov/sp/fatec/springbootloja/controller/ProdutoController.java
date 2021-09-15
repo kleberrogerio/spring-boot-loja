06261361ae91c50bc45562a5c24317fc5ad9ca77
KleberNascimento3 <kleber.nascimento3@etec.sp.gov.br>
2021-09-15 22:35:24 +0000

    Rest Alterações ProdutoController

--- src/main/java/br/gov/sp/fatec/springbootloja/controller/ProdutoController.java
+++ src/main/java/br/gov/sp/fatec/springbootloja/controller/ProdutoController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.gov.sp.fatec.springbootloja.entity.Marca;
 import br.gov.sp.fatec.springbootloja.entity.Produto;
 import br.gov.sp.fatec.springbootloja.service.ProdutoService;
 
@@ -30,5 +33,10 @@ public class ProdutoController {
     @GetMapping(value = "/nome")
     public Produto buscarProdutoPorNome(@RequestParam(value = "nome") String nome) {
         return produtoService.pesquisarPorNomeProduto(nome);        
+    }
+
+    @PostMapping
+    public Produto cadastrarNovoProduto(@RequestBody Produto produto,@RequestBody Marca marca){
+        return produtoService.cadastrarProduto(produto.getNome(), produto.getPreco(), marca.getNome());   
     }
 }

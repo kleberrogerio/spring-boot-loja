bf49c4d333a8df61f86a99023d2b36b3f5190cf9
KleberNascimento3 <kleber.nascimento3@etec.sp.gov.br>
2021-10-22 23:16:07 +0000

    Arrumando Bugs

--- src/main/java/br/gov/sp/fatec/springbootloja/controller/MarcaController.java
+++ src/main/java/br/gov/sp/fatec/springbootloja/controller/MarcaController.java
@@ -59,11 +59,13 @@ public class MarcaController {
                     return new ResponseEntity<Marca>(marca,responseHeaders,HttpStatus.CREATED);
     }
 
+    @JsonView(View.MarcaResumo.class)
     @PutMapping(value="/{id}")
     public Marca atualizarMarca(@PathVariable("id") Long id, @RequestBody Marca marca){
         return marcaService.atualizarMarca(id, marca.getNome());
     }
 
+    @JsonView(View.MarcaResumo.class)
     @DeleteMapping(value="/{id}")
     public void deletaMarca(@PathVariable("id") Long id) {
         marcaService.deleteMarca(id);    

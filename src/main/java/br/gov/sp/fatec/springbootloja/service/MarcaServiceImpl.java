d0fd38599702f0a4fc0784cce0ccda5841d9c17e
KleberNascimento3 <kleber.nascimento3@etec.sp.gov.br>
2021-10-25 15:46:51 +0000

    Protegendo os Métodos

--- src/main/java/br/gov/sp/fatec/springbootloja/service/MarcaServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootloja/service/MarcaServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import br.gov.sp.fatec.springbootloja.entity.Marca;
@@ -18,17 +19,20 @@ public class MarcaServiceImpl implements MarcaService{
 	private MarcaRepository marcaRepo;
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public void excluirMarca (Marca marca){
 		marcaRepo.delete(marca);	
     }
 	
     @Override
+    @PreAuthorize("isAuthenticated()")
 	public Marca pesquisarPorIdMarca (Long id){
         Marca marca = marcaRepo.findById(id).get();
         return marca;
     }
  
     @Override
+    @PreAuthorize("isAuthenticated()")
     public Marca atualizarMarca (Long id, String nome){
         Marca marca = marcaRepo.findById(id).get();
         if (marca != null){
@@ -40,11 +44,13 @@ public class MarcaServiceImpl implements MarcaService{
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")  
     public List<Marca> buscarTodasMarcas(){
         return marcaRepo.findAll();
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public Marca buscarMarcaPorId(Long id){
         Optional<Marca> marcaOp = marcaRepo.findById(id);
         if(marcaOp.isPresent()){
@@ -54,6 +60,7 @@ public class MarcaServiceImpl implements MarcaService{
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public Marca buscarMarcaPorNome(String nome){
         Marca marca = marcaRepo.findByNomeIgnoreCase(nome);
         if(marca !=null){
@@ -62,25 +69,18 @@ public class MarcaServiceImpl implements MarcaService{
         throw new RegistroNaoEncontradoException("Marca não encontrada");
     }
 
+    @Override
+    @PreAuthorize("isAuthenticated()")
     public Marca cadastrarNovaMarca(String nome){
         Marca marca = new Marca();
 		marca.setNome(nome);
         marcaRepo.save(marca);
         return marca; 
-    }  
-    
-    /*@Override
-    public void deleteMarca (Long id){
-        Optional<Marca> marcaOp = marcaRepo.findById(id);
-        if(marcaOp.isPresent()) {
-            marcaRepo.deleteById(id);
-        }else{
-        throw new RegistroNaoEncontradoException("Marca não encontrada!"); 
-        }
-			
-    }*/
+    }    
+   
 
      @Override
+     @PreAuthorize("isAuthenticated()")
     public void deleteMarca (Long id){
         Optional<Marca> marcaOp = marcaRepo.findById(id);
        
--- src/main/java/br/gov/sp/fatec/springbootloja/service/ProdutoServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootloja/service/ProdutoServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,6 +30,7 @@ public class ProdutoServiceImpl implements ProdutoService {
 	
 	@Override
 	@Transactional
+	@PreAuthorize("isAuthenticated()")
     public Produto cadastrarNovoProduto(String nome,Long idMarca, BigDecimal preco) {
         Marca marca = marcaService.buscarMarcaPorId(idMarca);
 		Produto produto = new Produto(); 
@@ -40,6 +42,7 @@ public class ProdutoServiceImpl implements ProdutoService {
     }
 
     @Override
+	@PreAuthorize("isAuthenticated()")
 	public void deleteProduto(Long id) {
 		Produto produto = produtoService.buscarProdutoPorId(id);		
 		if(produto != null) {
@@ -48,6 +51,7 @@ public class ProdutoServiceImpl implements ProdutoService {
 	}
 
 	@Override
+	@PreAuthorize("isAuthenticated()")
 	public Produto pesquisarPorNomeProduto(String nome) {
 		Produto produto = produtoRepo.findByNomeIgnoreCase(nome);
 		if(produto!=null){
@@ -58,6 +62,7 @@ public class ProdutoServiceImpl implements ProdutoService {
 	}
 
 	@Override
+	@PreAuthorize("isAuthenticated()")
 	public Produto buscarProdutoPorId(Long id) {
 		Optional<Produto> produtoOp = produtoRepo.findById(id);
 		if(produtoOp.isPresent()){
@@ -67,11 +72,13 @@ public class ProdutoServiceImpl implements ProdutoService {
 	}
 
 	@Override
+	@PreAuthorize("isAuthenticated()")
 	public List<Produto> pesquisarTodosProdutos() {
 		return produtoRepo.findAll();
 	}
 
 	@Override
+	@PreAuthorize("isAuthenticated()")
 	public Produto atualizarProduto(Long id,String nome,Long idMarca, BigDecimal preco) {
 		Marca marca = marcaService.buscarMarcaPorId(idMarca);
 		Produto produto = produtoRepo.findById(id).get();

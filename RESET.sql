ebb7a85d46c237ac7cb2320ee56a8c701568b5c3
KleberNascimento3 <kleber.nascimento3@etec.sp.gov.br>
2021-10-26 13:19:20 +0000

    Arrumando o SQL

--- RESET.sql
+++ RESET.sql
@@ -47,9 +47,11 @@ create table ite_itemvenda(
 create table usr_usuario (
     usr_id bigint unsigned not null auto_increment,
     usr_nome varchar(20) not null,
+    usr_email varchar(100) not null,
     usr_senha varchar(100) not null,
     primary key (usr_id),
-    unique key uni_usuario_nome (usr_nome)
+    unique key uni_usuario_nome (usr_nome),
+    unique key uni_usuario_email (usr_email)
  );
 
 create table aut_autorizacao (
@@ -73,7 +75,7 @@ create table uau_usuario_autorizacao (
 
 insert into mar_marca(mar_nome) values('DELL');
 insert into pro_produto(pro_nome, mar_id, pro_preco) values('DESKTOP', 1, 9500.00);
-insert into usr_usuario (usr_nome,usr_senha) values ('admin','$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
+insert into usr_usuario (usr_nome,usr_email,usr_senha) values ('admin','admin@email.com','$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
 insert into aut_autorizacao (aut_nome) values ('ROLE_ADMIN');
 insert into uau_usuario_autorizacao values (1,1);
 commit;

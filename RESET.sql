39ba8b39c43c851d83f439a6d405a9ef143c9563
KleberNascimento3 <kleber.nascimento3@etec.sp.gov.br>
2021-10-27 13:32:22 +0000

    Alterando o banco com usuário e autorização

--- RESET.sql
+++ RESET.sql
@@ -31,7 +31,7 @@ create table pro_produto(
 create table ven_venda(
 	ven_id bigint unsigned not null auto_increment,
 	ven_data date not null,
-        ven_qtde bigint not null,
+    ven_qtde bigint not null,
   	ven_preco_venda decimal(15,2) not null,
 	primary key (ven_id)
 );
@@ -44,6 +44,38 @@ create table ite_itemvenda(
   foreign key ite_ven_fk (ven_id) references ven_venda (ven_id) on delete restrict on update cascade
 );
 
+create table usr_usuario (
+    usr_id bigint unsigned not null auto_increment,
+    usr_nome varchar(20) not null,
+    usr_email varchar(100) not null,
+    usr_senha varchar(100) not null,
+    primary key (usr_id),
+    unique key uni_usuario_nome (usr_nome),
+    unique key uni_usuario_email (usr_email)
+ );
+
+create table aut_autorizacao (
+    aut_id bigint unsigned not null auto_increment,
+    aut_nome varchar(20) not null,
+    primary key (aut_id),
+    unique key uni_aut_nome (aut_nome)
+);
+
+create table uau_usuario_autorizacao (
+    usr_id bigint unsigned not null,
+    aut_id bigint unsigned not null,
+    primary key (usr_id, aut_id),
+    foreign key uau_usr_fk (usr_id)
+        references usr_usuario (usr_id)
+        on delete restrict on update cascade,
+    foreign key uau_aut_fk (aut_id)
+        references aut_autorizacao (aut_id)
+        on delete restrict on update cascade
+);
+
 insert into mar_marca(mar_nome) values('DELL');
 insert into pro_produto(pro_nome, mar_id, pro_preco) values('DESKTOP', 1, 9500.00);
+insert into usr_usuario (usr_nome,usr_email,usr_senha) values ('admin','admin@email.com','$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
+insert into aut_autorizacao (aut_nome) values ('ROLE_ADMIN');
+insert into uau_usuario_autorizacao values (1,1);
 commit;

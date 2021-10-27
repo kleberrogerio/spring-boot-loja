6b90e957e3534c8383b4d4f5d5354775f7509b0b
KleberNascimento3 <kleber.nascimento3@etec.sp.gov.br>
2021-10-27 18:42:32 +0000

    Inserindo testes

--- src/test/java/br/gov/sp/fatec/springbootloja/repository/AutorizacaoRepositoryTests.java
+++ src/test/java/br/gov/sp/fatec/springbootloja/repository/AutorizacaoRepositoryTests.java
@@ -1,15 +1,17 @@
 package br.gov.sp.fatec.springbootloja.repository;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.HashSet;
+
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.sp.fatec.springbootloja.entity.Autorizacao;
 import br.gov.sp.fatec.springbootloja.entity.Usuario;
@@ -17,35 +19,13 @@ import br.gov.sp.fatec.springbootloja.entity.Usuario;
 @SpringBootTest
 @Transactional
 @Rollback
-public class UsuarioRepositoryTests {
-
+public class AutorizacaoRepositoryTests {
+    
     @Autowired
     private UsuarioRepository usuarioRepo;
-
+    
     @Autowired
     private AutorizacaoRepository autorizacaoRepo;
-
-    @Test
-    void findByNomeTest(){
-        Usuario usuario = new Usuario();
-        usuario.setNome("Teste");
-        usuario.setEmail("teste@email.com");
-        usuario.setSenha("123");
-        usuarioRepo.save(usuario);
-
-        assertNotNull(usuarioRepo.findByNome("Teste"));
-    }
-
-    @Test
-    void findByNomeContainsOrEmailContains(){
-        Usuario usuario = new Usuario();
-        usuario.setNome("Teste");
-        usuario.setEmail("teste@email.com");
-        usuario.setSenha("123");
-        usuarioRepo.save(usuario);
-
-        assertFalse(usuarioRepo.findByNomeContainsOrEmailContains("est", "lalala").isEmpty());
-    }
 
     @Test
     void findByAutorizacoesNomeTest(){
@@ -62,8 +42,7 @@ public class UsuarioRepositoryTests {
         usuario.getAutorizacoes().add(autorizacao);
         usuarioRepo.save(usuario);
 
-        assertFalse(usuarioRepo.findByAutorizacoesNome("Teste").isEmpty());
+        assertFalse(autorizacaoRepo.findByUsuariosNome("Teste").isEmpty());
     }
 
-    
 }

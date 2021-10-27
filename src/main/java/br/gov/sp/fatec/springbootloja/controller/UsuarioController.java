99886c1e45f5bdb2fced6f687dfd5b16da7ef68b
KleberNascimento3 <kleber.nascimento3@etec.sp.gov.br>
2021-10-27 21:51:43 +0000

    Inserindo o Usuario Controller

---
+++ src/main/java/br/gov/sp/fatec/springbootloja/controller/UsuarioController.java
@@ -0,0 +1,18 @@
+package br.gov.sp.fatec.springbootloja.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//import br.gov.sp.fatec.armazem.service.SegurancaService;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping(value="/usuario")
+public class UsuarioController{
+
+    //private SegurancaService segurancaService;
+
+
+}

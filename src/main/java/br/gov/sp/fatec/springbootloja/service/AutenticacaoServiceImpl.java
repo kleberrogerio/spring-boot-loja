8018f621f653a13f052b3902280e3a6a63677f26
jujalu <jujalu@gmail.com>
2021-10-30 03:59:44 +0000

    Video 3 security finalizado

--- src/main/java/br/gov/sp/fatec/springbootloja/service/AutenticacaoServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootloja/service/AutenticacaoServiceImpl.java
@@ -2,6 +2,7 @@ package br.gov.sp.fatec.springbootloja.service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.gov.sp.fatec.springbootloja.entity.Autorizacao;
 import br.gov.sp.fatec.springbootloja.entity.Usuario;
+import br.gov.sp.fatec.springbootloja.exception.RegistroNaoEncontradoException;
 import br.gov.sp.fatec.springbootloja.repository.AutorizacaoRepository;
 import br.gov.sp.fatec.springbootloja.repository.UsuarioRepository;
 
@@ -51,12 +53,42 @@ public class AutenticacaoServiceImpl implements AutenticacaoService {
     }
 
     @Override
-    @PreAuthorize("isAuthenticated()") 
+    @PreAuthorize("hasRole('ADMIN')")
     public List<Usuario> buscarTodosUsuarios() {
 
         return usuarioRepo.findAll();
-
     }
+
+    @Override
+    @PreAuthorize("isAuthenticated()")
+    public Usuario buscarUsuarioPorNome(String nome) {
+      Usuario usuario = usuarioRepo.findByNome(nome);
+      if (usuario != null) {
+        return usuario;
+      }
+      throw new RegistroNaoEncontradoException("Usuário não encontrado!");
+    }
+
+    @Override
+    @PreAuthorize("hasAnyRole('ADMIN', 'USUARIO')")
+    public Usuario buscarUserById(Long id) {
+      Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
+      if (usuarioOp.isPresent()) {
+        return usuarioOp.get();
+      }
+      throw new RegistroNaoEncontradoException("Usuário não encontrado!");
+    }
+
+    @Override
+    @PreAuthorize("isAuthenticated()")
+    public Autorizacao buscarAutorizacaoPorNome(String nome) {
+      Autorizacao autorizacao = autorizacaoRepo.findByNome(nome);
+      if (autorizacao != null) {
+        return autorizacao;
+      }
+      throw new RegistroNaoEncontradoException("Autorização não encontrada!");
+    }
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -64,7 +96,6 @@ public class AutenticacaoServiceImpl implements AutenticacaoService {
         if (usuario == null) {
             throw new UsernameNotFoundException("Usuário " + username + " não encontrado! ");
         }
-        //System.out.println(usuario.getAutorizacoes());
         return User.builder().username(username).password(usuario.getSenha())
                 .authorities(usuario.getAutorizacoes().stream()
                     .map(Autorizacao::getNome).collect(Collectors.toList())
